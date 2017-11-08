9221693e08cce35c51fb93c8f964e7ebe63b8383
xqq <zhuhaoqi@gisiinfo.com>
2017-11-08 16:36:21 +0800

    init

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
-import printMe from './print.js';
-import "./style.css";
+// import printMe from "./print.js"
+import "./style.css"
+import img from "./images/background.jpg"
 // console.log(css)
-function componment() {
-	var element = document.createElement('p');
-	element.innerHTML = 'awesome';
+/* function componment() {
+	var element = document.createElement("p")
+	element.innerHTML = "awesome"
 
 	return element
-}
+} */
+console.log(img)
+
+let [a, b, c] = [1, 2, 3]
+console.log(`a:${a} b:${b} c:${c}`)
+
 // document.body.appendChild(componment());
 if (module.hot) {
-	module.hot.accept('./print.js', function() {
+	module.hot.accept("./print.js", function() {
 		// document.body.appendChild("<p>awesome</p>");
 	})
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-var path = require('path');
-var glob = require('glob');
+var path = require("path");
+var glob = require("glob");
 
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var PurifyCSSPlugin = require('purifycss-webpack');
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
+var PurifyCSSPlugin = require("purifycss-webpack");
 
 module.exports = function(env) {
     var isPrd = function() {
-        return env != 'prd';;
+        return env != "prd";
     };
     var cssLoader = {
-        loader: 'css-loader',
+        loader: "css-loader",
         options: {
-            // root: './src/images', //适用于@import
+            // root: "./src/images", //适用于@import
             url: false,
             minimize: isPrd()
         }
@@ -25,13 +25,22 @@ module.exports = function(env) {
             filename: "bundle.js",
             path: path.resolve(__dirname, "dist")
         },
-        devtool: 'inline-source-map',
+        devtool: "inline-source-map",
         devServer: {
-            // contentBase: '.',
+            // contentBase: ".",
             hot: true
         },
         module: {
             rules: [{
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["babel-preset-env"]
+                    }
+                }
+            }, {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
@@ -39,28 +48,28 @@ module.exports = function(env) {
                 })
             }, {
                 test: /\.(png|jpg|gif)$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {}  
-                  }
-                ]
-              }]
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        name: "[hash].[ext]",
+                    }
+                }]
+            }]
         },
         plugins: [
             new CleanWebpackPlugin(["dist"]),
             new HtmlWebpackPlugin({
-                // title: 'Hot Module Replacement',
-                template: 'index.html'
+                // title: "Hot Module Replacement",
+                template: "index.html"
             }),
             new webpack.NamedModulesPlugin(),
             new webpack.HotModuleReplacementPlugin(),
             new ExtractTextPlugin({
-                filename: '[name].[contenthash].css',
+                filename: "[name].[contenthash].css",
                 allChunks: false
             }),
             new PurifyCSSPlugin({
-                paths: glob.sync(path.join(__dirname, 'index.html')),
+                paths: glob.sync(path.join(__dirname, "index.html")),
             })
         ]
     }
@@ -68,9 +77,9 @@ module.exports = function(env) {
 
 /*isPrd() ?
     ExtractTextPlugin.extract({
-        fallback: 'style-loader',
+        fallback: "style-loader",
         use: [cssLoader]
     }) : [
-        'style-loader',
+        "style-loader",
         cssLoader
     ]*/
